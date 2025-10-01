aac631fe469522410b8fc946003ecd482ecb5e9a
PedroCesar-2805 <cesarpedro159@gmail.com>
2025-09-30 23:05:19 -0300

    Criando a pagina registar

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
-
+     const navigate = useNavigate();
     const handleSubmit = async (e) => {
         e.preventDefault();
         setError('');
@@ -16,7 +16,9 @@ const Login = () => {
             setError('Email ou senha inválidos.');
         }
     };
-
+    const handleRegistrar = () => {
+    navigate('/Registrar');
+  };
     return (
         <div style={{ maxWidth: 400, margin: '60px auto', padding: 24, border: '1px solid #ccc', borderRadius: 8 }}>
             <h2>Login</h2>
@@ -42,8 +44,11 @@ const Login = () => {
                     />
                 </div>
                 {error && <div style={{ color: 'red', marginBottom: 12 }}>{error}</div>}
-                <button type="submit" style={{ width: '100%', padding: 10, background: '#1976d2', color: '#fff', border: 'none', borderRadius: 4 }}>
+                <button type="submit" style={{ width: '100%', padding: 10, background: '#1976d2', color: '#fff', border: 'none', borderRadius: 4, marginBottom: 10 }}>
                     Entrar
+                </button>
+                <button type="submit"  onClick={handleRegistrar} style={{ width: '100%', padding: 10, background: '#1976d2', color: '#fff', border: 'none', borderRadius: 4 }}>
+                    Registrar
                 </button>
             </form>
         </div>
